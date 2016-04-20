58ec302a3a7311054656e6526d23ba2fc52e05dc
Dario <dariojahic95@gmail.com>
2016-04-20 15:58:50 +0200

    Napravljena gui klasa.

---
+++ RezervacijeSala/src/gui/RezSalaGUI.java
@@ -0,0 +1,80 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Dimension;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+
+public class RezSalaGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel panel;
+	private JScrollPane scrollPane;
+	private JTable table;
+	private JLabel lblTipSale;
+	private JComboBox comboBox;
+
+	/**
+	 * Launch the application.
+	 */
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public RezSalaGUI() {
+		setTitle("Rezervacija sala");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(getPanel(), BorderLayout.EAST);
+		contentPane.add(getScrollPane(), BorderLayout.CENTER);
+	}
+
+	private JPanel getPanel() {
+		if (panel == null) {
+			panel = new JPanel();
+			panel.setPreferredSize(new Dimension(100, 10));
+			panel.add(getLblTipSale());
+			panel.add(getComboBox());
+		}
+		return panel;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setViewportView(getTable());
+		}
+		return scrollPane;
+	}
+	private JTable getTable() {
+		if (table == null) {
+			table = new JTable();
+		}
+		return table;
+	}
+	private JLabel getLblTipSale() {
+		if (lblTipSale == null) {
+			lblTipSale = new JLabel("Tip sale:");
+		}
+		return lblTipSale;
+	}
+	private JComboBox getComboBox() {
+		if (comboBox == null) {
+			comboBox = new JComboBox();
+			comboBox.setModel(new DefaultComboBoxModel(new String[] {"Amfiteatar", "RC", "Ucionica"}));
+		}
+		return comboBox;
+	}
+}
