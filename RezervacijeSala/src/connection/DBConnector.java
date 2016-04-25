ccdd355f31064f516658adf71c8cebd48efea377
duleignjatovic995 <dusan.ignjatovic@hotmail.com>
2016-04-25 19:32:25 +0200

    Odradjen javadoc za klasu DBConnector

--- RezervacijeSala/src/connection/DBConnector.java
+++ RezervacijeSala/src/connection/DBConnector.java
@@ -3,11 +3,20 @@ package connection;
 import java.sql.*;
 
 public class DBConnector {
+	/**
+	 * Glavni atribut klase - predstavlja samu konekciju
+	 */
 	Connection conn = null;
+	
 	String url = "jdbc:mysql://localhost:3306/sale_v2?autoReconnect=true&useSSL=false";
 	String user = "root";
 	String password = "Sennheiser95";
 
+	/**
+	 * Metoda vraca novu konekciju
+	 * @return konekcija
+	 * @throws java.sql.SQLException
+	 */
 	public Connection connect() throws SQLException {
 		conn = DriverManager.getConnection(url, user, password);
 		return conn;
